65ac4a07a9babc6c981adf7109a039a15351d504
Charlie <charlie.feng@mail.mcgill.ca>
2015-11-23 12:25:13 -0500

    browse.js

--- scripts/controllers/browse.js
+++ scripts/controllers/browse.js
@@ -5,4 +5,9 @@
 
 app.controller('BrowseController', function($scope, $routeParams, toaster, Task, Auth) {
 
+	$scope.searchTask = '';
+	$scope.tasks = Task.all;
+	$scope.signedIn = Auth.signedIn;
+	$scope.listMode = true;
+
 });
