c5503189e738dad37f61f3f3138798ac3cc09cd9
Charlie <charlie.feng@mail.mcgill.ca>
2015-11-23 16:05:52 -0500

    added comment.js

---
+++ scripts/services/comment.js
@@ -0,0 +1,29 @@
+/*jslint node: true */
+/*global angular*/
+/*global app*/
+/*global Firebase*/
+'use strict';
+
+app.factory('Comment', function(FURL, $firebase) {
+
+	var ref = new Firebase(FURL);
+
+	var Comment = {
+		comments: function(taskId) {
+			return $firebase(ref.child('comments').child(taskId)).$asArray();
+		},
+
+		addComment: function(taskId, comment) {
+			var task_comments = this.comments(taskId);
+			comment.datetime = Firebase.ServerValue.TIMESTAMP;
+
+			if(task_comments) {
+				return task_comments.$add(comment);
+			}
+		}
+
+	};
+
+	return Comment;
+
+});
